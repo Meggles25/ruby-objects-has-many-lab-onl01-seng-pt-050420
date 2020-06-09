58aa15b0247b19f3ea5ff06d497925fe5a740d13
Megan Gomes <megan.gomes25@gmail.com>
2020-06-09 22:40:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,8 +17,8 @@ class Author
     @@post_count += 1
   end
   
-  def add_post_by_name(post_name)
-    post = Post.new(post_name)
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
     add_post(post)
     post.author = self
   end
